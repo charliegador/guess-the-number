c2eb81d07ba0d2839a1b12442a95466e51c2281b
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-11-02 15:24:08 +0100

    step 2

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Read instructions. And remove this line.
+let secretNumber = randomInt(100);
+
 function randomInt(max){
     return Math.floor(Math.random() * Math.floor(max));
 }
 console.log(randomInt(100));
 console.log(randomInt(20));
+
+function getUserGuess(){
+    let guess = document.getElementById('user-input').value;
+    return guess
+} 
+globalThis.getUserGuess = getUserGuess
