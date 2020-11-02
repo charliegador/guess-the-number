ad1d9a3d5d6d6377532ba30f5eceb81c45f0e705
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-11-02 14:06:57 +0100

    step 1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Read instructions. And remove this line.
+function randomInt(max){
+    return Math.floor(Math.random() * Math.floor(max));
+}
+console.log(randomInt(100));
+console.log(randomInt(20));
