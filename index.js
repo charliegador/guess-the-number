0e5da4787da2a58054171cf5587399bbbe058966
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-11-09 14:47:37 +0100

    Blandat

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-// Read instructions. And remove this line.
+// Part 1
 
 function randomInt(max){
     return Math.floor(Math.random() * Math.floor(max));
 }
-console.log(randomInt(100));
-console.log(randomInt(20));
-
 const secretNumber = randomInt(100);
 
+console.log(secretNumber);
+// Part 2
 function getUserGuess(){
     const guess = document.getElementById('user-input').value;
     return parseInt(guess, 10);
 } 
 globalThis.getUserGuess = getUserGuess;
-
+// Part 3
 document.addEventListener('keyup', keyPress);
 
 function keyPress(enter){
     if(enter.key == "Enter"){
-        console.log(getUserGuess())
+        console.log(getUserGuess() + " " + secretNumber);
+        // Part 4
+        if (getUserGuess() < secretNumber){
+            setMessage("Too low!");  
+        }
+        else if (getUserGuess() > secretNumber){
+            setMessage("Too High!");
+        }
+        else {
+            setMessage("You are correct!");
+        }
+        guesses.push(getUserGuess());
+        console.log(guesses);
+        clearInput();
     }
-} 
+    
+}
+
+function setMessage(msg){
+    console.log("setMessage")
+    let textfield = document.getElementById('message');
+    textfield.innerHTML = msg;
+}
+// Part 5
+function clearInput(){
+    document.getElementById('user-input').value = "";
+
+}
+
+//Fördjupning
+const guesses = []
+
